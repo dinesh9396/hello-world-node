49e6c7a79060a4acab00c619e3d8fecc378aa3ac
dinesh9396 <84271102+dinesh9396@users.noreply.github.com>
2023-07-20 18:21:05 +0530

    Create Helloworld.js

---
+++ Helloworld.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+const server = http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(3000);
